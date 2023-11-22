d27c5206202f356580a10f94bb4e5dc6673ba385
mohamadmorady412 <mohamadmorady412@gmail.com>
2023-11-22 05:26:05 -0800

    مشکلی پیش آمده بود رفع شد
    به یاد قناری سکته کرده ریواس دون طلایی

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,9 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 
-    'website.apps.WebsiteConfig'
 ]
 
 MIDDLEWARE = [
--- website/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse , JsonResponse
-
-def http_test(request):
-    return HttpResponse('<h1>hello</h1>')
-
-def json_test(request):
-    return  JsonResponse({'ali':'salam','karim':'benzema'})
